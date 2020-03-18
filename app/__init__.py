e7aa856a9cd5f236c81aafdf99e028ca752b35ee
Rodrigo_Alejandro <mefistoalejandro@hotmail.com>
2020-03-18 00:51:54 -0300

    Imagenes y detalles

--- app/__init__.py
+++ app/__init__.py
@@ -6,13 +6,13 @@ login_manager = LoginManager()
 db = SQLAlchemy()
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_url_path="/" )
     app.config['DEBUG'] = True
     app.config['TESTING'] = True
     app.config['SECRET_KEY'] = '7110c8ae51a4b5af97be6534caef90e4bb9bdcb3380af008f90b23a5d1616bf319bc298105da20fe'
     #APP CONFIG DE LA BASE DE DATOS EN MAQUINA VIRTUAL DANI
-    #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://bravo:bravo@179.9.115.60/software'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://moran:moran@192.168.0.250/software'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://bravo:bravo@179.9.115.60/software'
+    #app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://moran:moran@192.168.0.250/software'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
     app.config['USE_SESSION_FOR_NEXT']=True
     app.config['SQLALCHEMY_POOL_SIZE']=100
